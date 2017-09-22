b8d1fd7cc1d51f702a5feea28aeed8e0398ee4dd
carlos <bobcn_1991@hotmail.com>
2017-09-22 10:09:58 +0100

    MessageList and Message (tweet) Components

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+import 'normalize-css'
+import styles from './app.css'
 import Header from '../Header'
 import Main from '../Main'
 
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,8 +1,30 @@
 import React, { Component } from 'react'
+import MessageList from '../MessageList'
 
 class Main extends Component{
+    constructor(){
+        super()
+        this.state = {
+            messages: [{
+                text: 'Tweet test',
+                picture:'',
+                displayName:'Carlos',
+                username:'carlosalvarez',
+                date: Date.now()
+            },
+            {
+                text: 'Tweet test 2',
+                picture:'',
+                displayName:'Carlos',
+                username:'carlosalvarez',
+                date: Date.now()
+            }]
+        }
+    }
     render(){
-        return <div>Test</div>
+        return (
+            <MessageList messages={this.state.messages}/>
+        )
     }
 }
 export default Main
