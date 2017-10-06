5ad98d095eae34b00523d06720e7b0852a3351b5
carlos <bobcn_1991@hotmail.com>
2017-10-06 14:18:02 +0100

    Firebase init

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 import {render} from 'react-dom'
 import App from './components/App'
+import firebase from 'firebase'
+
+firebase.initializeApp({
+    apiKey: 'AIzaSyBmvf6ET4re72L6mSHbSQSuXG6XPSGxOGk',
+    authDomain: 'twitter-clone-fc841.firebaseapp.com',
+    databaseURL: 'https://twitter-clone-fc841.firebaseio.com',
+    projectId: 'twitter-clone-fc841',
+    storageBucket: '',
+    messagingSenderId: '47981543161'
+});
 
 render(<App />, document.getElementById('root'))
