9112b176b1ebba4247d590bc4a43ca35a13ad5b5
carlos <bobcn_1991@hotmail.com>
2017-10-06 14:05:14 +0100

    Link to 'Home' and Loading... message

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router'
 import styles from './header.css'
 
 // This is a Stateless Component, so we don't need to use a react component
 function Header() {
         return(
             <header className={styles.root}>
-                <h1 className={styles.logo}>
-                    Test
-                </h1>
+                <Link to={`/`} style={{ textDecoration: 'none' }}>
+                    <h1 className={styles.logo}>
+                        TEST
+                    </h1>
+                </Link>
             </header>
         )
 }
